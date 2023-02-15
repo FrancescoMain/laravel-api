17f103acc57966617c322f2411d8d97deffc9a3e
francesco <cesaranofrancescomain@gmail.com>
2023-02-15 14:57:21 +0100

    factory

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            
+
+        'name' => fake() -> words(2, true), 
+        'year' => fake() -> yar(), 
+        'cashOut' => fake() -> numberBetween(0 , 100000) , 
+
+    
+        ];
+    }
+}
