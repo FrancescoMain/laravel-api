f2957cc1b1f8425d2a8719fe4e9bb830bd134335
Francesco <cesaranofrancescomain@gmail.com>
2023-02-16 17:43:15 +0100

    test api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Controllers\ApiController;
+
+Route :: get ('/api/v1/test', [ApiController :: class, 'test']);
--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,7 @@ Route :: post('/movie/edit/{movie}', [MainController :: class, 'movieUpdate'])
 
 Route :: get('/movie/delete/{movie}', [MainController :: class, 'movieDelete'])
     ->name('movie.delete');    
+
+ use App\Http\Controllers\ApiController;
+
+Route :: get ('/api/v1/movie/all', [ApiController :: class, 'movieAll']);   
