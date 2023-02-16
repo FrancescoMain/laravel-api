20ec1f76c021dfdd54ab570b6476ff796daf7291
Francesco <cesaranofrancescomain@gmail.com>
2023-02-16 18:05:28 +0100

    front end

---
+++ frontend/vue-laravel-microservices/src/App.vue
@@ -0,0 +1,29 @@
+<script >
+
+  import Axios from 'axios' ;
+
+  export default {
+
+    mounted() {
+      Axios.get('http://127.0.0.1:8000/api/v1/movie/all')
+        .then(res => {
+
+          const data = res.data;
+          const success = data.success;
+          const movies = data.response;
+          console.log(movies);
+        })
+        .catch(err => console.error(err));
+    }
+  }
+</script>
+
+<template>
+    <h1>
+      hello world
+    </h1>
+</template>
+
+<style scoped>
+
+</style>
