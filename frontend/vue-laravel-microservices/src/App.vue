a63ea052816fc5242a5274d9c7a6c469e6f0228e
Francesco <cesaranofrancescomain@gmail.com>
2023-02-20 16:11:10 +0100

    all movie

--- frontend/vue-laravel-microservices/src/App.vue
+++ frontend/vue-laravel-microservices/src/App.vue
@@ -1,29 +1,79 @@
 <script >
 
-  import Axios from 'axios' ;
+  import axios from 'axios';
+
+  const API_URL = 'http://127.0.0.1:8000/api/';
+  const API_VER = 'v1/';
+  const API = API_URL + API_VER;
 
   export default {
+    data() {
+      return {
+      
+        movies: [],
+        genres: [],
+        tags: [],
 
+        movie_name: '',
+        movie_year: '',
+        movie_cashOut: '',
+
+        movie_genre: '',
+        movie_tags: [],
+      }
+    },
+    methods: {
+      updateData() {
+        axios.get(API + 'movie/all')
+          .then(res => {
+            const data = res.data;
+            const success = data.success;
+
+            const response = data.response;
+
+            const movies =  response.movies;
+            const genres = response.genres;
+            const tags = response.tags;
+
+
+            if (success) {
+              
+              this.movies = movies;
+              this.genres = genres;
+              this.tags = tags;
+              
+            }
+          })
+          .catch(err => console.error(err));
+
+      },
+      takeGenre(id) {
+        return genres[id];
+      }
+    },
     mounted() {
-      Axios.get('http://127.0.0.1:8000/api/v1/movie/all')
-        .then(res => {
-
-          const data = res.data;
-          const success = data.success;
-          const movies = data.response;
-          console.log(movies);
-        })
-        .catch(err => console.error(err));
+      this.updateData();
+      console.log(this.Data);
     }
   }
 </script>
 
 <template>
     <h1>
-      hello world
+      Movies
     </h1>
+    <ul>
+      <li v-for="movie in movies" :key="movie.id">
+        {{movie.name}}
+        <ul>
+          <li v-for="tag in movie.tags" :key="tag.id">
+            {{ tag.name }}
+          </li>
+        </ul>
+      </li>  
+    </ul>
 </template>
 
-<style scoped>
+<style>
 
 </style>
--- routes/web.php
+++ routes/web.php
@@ -35,4 +35,4 @@ Route :: get('/movie/delete/{movie}', [MainController :: class, 'movieDelete'])
 
  use App\Http\Controllers\ApiController;
 
-Route :: get ('/api/v1/movie/all', [ApiController :: class, 'movieAll']);   
+Route :: get ('/api/v1/movie/all', [ApiController :: class, 'getMovieWTagWGenre']);   
